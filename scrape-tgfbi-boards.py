45ee1c4118e9b65f9151b965ae74cbecf2ab67a9
michael-petersen <petersen.michael.s@gmail.com>
2026-03-09 13:54:17 +0000

    Updated TGFBI boards

--- scrape-tgfbi-boards.py
+++ scrape-tgfbi-boards.py
@@ -22,7 +22,7 @@ def scrape_drafts(leagues):
         # league = 810
         driver.get(url.format(league))
         #time.sleep(np.random.randint(low = 20, high = 25, size = 1)[0])
-        time.sleep(np.random.randint(low = 4, high = 8, size = 1)[0])
+        time.sleep(np.random.randint(low = 8, high = 12, size = 1)[0])
         
         html = driver.page_source
         soup = BeautifulSoup(html)
